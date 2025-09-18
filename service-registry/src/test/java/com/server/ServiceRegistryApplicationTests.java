8a806fe28a8cff64fefe0c6499585f5bb6232234
u22ss <howareyou2212@gmail.com>
2025-09-18 23:05:30 +0530

    Update ServiceRegistryApplicationTests.java

--- service-registry/src/test/java/com/server/ServiceRegistryApplicationTests.java
+++ service-registry/src/test/java/com/server/ServiceRegistryApplicationTests.java
@@ -2,6 +2,8 @@ package com.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java .util*;
+
 
 @SpringBootTest
 class ServiceRegistryApplicationTests {
